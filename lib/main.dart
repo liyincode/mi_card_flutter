aef2cdcf46f1fb67d02c320f1c2338094c9cff45
Shuaiyin <shuaiyin.top@foxmai..com>
2020-04-28 08:06:24 +0800

    how to use icon and build my simple profile

--- lib/main.dart
+++ lib/main.dart
@@ -21,22 +21,66 @@ class MyApp extends StatelessWidget {
               Text(
                 'Shuaiyin',
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Pacifico'
-                ),
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
               ),
               Text(
                 'FLUTTER DEVELOPER',
                 style: TextStyle(
-                  fontFamily: 'Source sans pro',
-                  color: Colors.teal.shade100,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold
+                    fontFamily: 'Source sans pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '168 123 456 789',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source sans pro',
+                          fontSize: 20.0),
+                    )
+                  ],
                 ),
-              )
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'shuaiyin.xxx@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source sans pro',
+                          fontSize: 20.0),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
